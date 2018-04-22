fbd2900112ac8227035714d197f24481ab98732b
Kerman <kermankohli@gmail.com>
2018-04-22 14:43:40 +1000

    Using official Zepelin smart contract for Ownabl

--- contracts/base/Ownable.sol
+++ contracts/base/Ownable.sol
@@ -1,11 +1,11 @@
-pragma solidity ^0.4.3;
+pragma solidity ^0.4.21;
+
 
 /**
  * @title Ownable
  * @dev The Ownable contract has an owner address, and provides basic authorization control
  * functions, this simplifies the implementation of "user permissions".
  */
-
 contract Ownable {
     address public owner;
 
@@ -18,27 +18,25 @@ contract Ownable {
     * account.
     */
     function Ownable() public {
-        owner = msg.sender;
+      owner = msg.sender;
     }
-
 
     /**
     * @dev Throws if called by any account other than the owner.
     */
     modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
+      require(msg.sender == owner);
+      _;
     }
-
 
     /**
     * @dev Allows the current owner to transfer control of the contract to a newOwner.
     * @param newOwner The address to transfer ownership to.
     */
-    function transferOwnership(address newOwner) onlyOwner public {
-        require(newOwner != address(0));
-        emit OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
+    function transferOwnership(address newOwner) public onlyOwner {
+      require(newOwner != address(0));
+      emit OwnershipTransferred(owner, newOwner);
+      owner = newOwner;
     }
 
 }
