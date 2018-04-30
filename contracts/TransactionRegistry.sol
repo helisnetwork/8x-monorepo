d10b6ce82a74b9569b9109d6b74d000d9dd0861e
Kerman <kermankohli@gmail.com>
2018-04-30 19:27:49 +1000

    Skeleton TransactionRegistry added

---
+++ contracts/TransactionRegistry.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.18;
+
+/** @title Transaction Registry - Stores a list of all the currently pending transactions */
+/** @author Kerman Kohli - <kerman@TBD.com> */
+
+contract TransactionRegistry {
+    
+}
