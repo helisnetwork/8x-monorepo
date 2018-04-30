2f212c7e8c7d1d97be6653a19d4c89e032963014
Kerman <kermankohli@gmail.com>
2018-04-30 20:11:47 +1000

    Simplified inheritance structure

--- contracts/base/token/StandardToken.sol
+++ contracts/base/token/StandardToken.sol
@@ -1,8 +1,7 @@
 pragma solidity ^0.4.21;
 
-import "./BasicToken.sol";
+import "../math/SafeMath.sol";
 import "./ERC20.sol";
-
 
 /**
  * @title Standard ERC20 token
@@ -11,9 +10,46 @@ import "./ERC20.sol";
  * @dev https://github.com/ethereum/EIPs/issues/20
  * @dev Based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
  */
-contract StandardToken is ERC20, BasicToken {
+contract StandardToken is ERC20 {
+
+    using SafeMath for uint256;
+
+    mapping(address => uint256) balances;
+
+    uint256 totalSupply_;
 
     mapping (address => mapping (address => uint256)) internal allowed;
+
+     /**
+    * @dev total number of tokens in existence
+    */
+    function totalSupply() public view returns (uint256) {
+        return totalSupply_;
+    }
+
+    /**
+    * @dev transfer token for a specified address
+    * @param _to The address to transfer to.
+    * @param _value The amount to be transferred.
+    */
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0));
+        require(_value <= balances[msg.sender]);
+
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    /**
+    * @dev Gets the balance of the specified address.
+    * @param _owner The address to query the the balance of.
+    * @return An uint256 representing the amount owned by the passed address.
+    */
+    function balanceOf(address _owner) public view returns (uint256) {
+        return balances[_owner];
+    }
 
 
     /**
