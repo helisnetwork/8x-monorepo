220faf5ad62304b2274c07c79971b0b21121c907
Kerman <kermankohli@gmail.com>
2018-05-05 19:43:46 +1000

    Transaction Registry solidity skeleton added

--- contracts/TransactionRegistry.sol
+++ contracts/TransactionRegistry.sol
@@ -1,8 +1,72 @@
 pragma solidity ^0.4.18;
 
+import "./Authorizable.sol";
+
 /** @title Transaction Registry - Stores a list of all the currently pending transactions */
 /** @author Kerman Kohli - <kerman@TBD.com> */
 
-contract TransactionRegistry {
+contract TransactionRegistry is Authorizable {
+
+    struct Payment {
+        address subscriptionContract;
+
+        uint duedate;
+        uint lastPaymentDate;
+
+        address claimant;
+        uint stake;
+        uint claimDate;
+    }
+
+    mapping (bytes32 => Payment) public payments;
+
+    /**
+      * Modifiers
+    */
+
+    /**
+      * Public functions
+    */
+
+    function createNewPayment(
+        bytes32 _subscriptionIdentifier, 
+        address _subscriptionContract,
+        uint _duedate,
+        uint _stake)
+    
+        public
+        onlyAuthorized
+        returns (bool _success)
+    {
+
+        // @TODO: Implementation
+
+    }
+
+    function claimPayment(bytes32 _subscriptionIdentifier) public onlyAuthorized {
+
+        // @TODO: Implementation
+
+    }
+
+    function executePayment(bytes32 _subscriptionIdentifier) public onlyAuthorized {
+
+        // @TODO: Implementation
+
+    }
+
+    function updateForNextPayment(
+        bytes32 _subscriptionIdentifier, 
+        uint _duedate, 
+        uint _stake
+    ) 
+        public 
+        onlyAuthorized 
+        returns (bool _success)
+    {
+
+        // @TODO: Implementation
+
+    }
     
 }
