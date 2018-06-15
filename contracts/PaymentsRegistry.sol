502151b522f3110d6247e73c8b635750a07a84d9
Kerman <kermankohli@gmail.com>
2018-06-15 13:49:46 +1000

    Minor changes to PaymentsRegistry required variables

--- contracts/PaymentsRegistry.sol
+++ contracts/PaymentsRegistry.sol
@@ -9,10 +9,10 @@ contract PaymentsRegistry is Authorizable {
 
     struct Payment {
         address subscriptionContract;
+        bytes32 subscriptionIdentifier;
 
         uint dueDate;
         uint lastPaymentDate;
-        uint stake;
 
         address claimant;
         uint executionPeriod;
@@ -40,9 +40,9 @@ contract PaymentsRegistry is Authorizable {
 
     }
 
-    /** @dev Create a new payment object when a user initially subscribes to a plan.
+    /** @dev Create a new payment object when a user intially subscribes to a plan.
       * @param _subscriptionContract is the contract where the details exist (adheres to Collectable contract interface).
-      * @param _subscriptionIdentifier is the identifier o f that customer's subscription with its relevant details.
+      * @param _subscriptionIdentifier is the identifier o f that customer's subscription with it's relevant details.
       * @param _dueDate is when the payment is meant to be paid by.
       * @param _stake is how much the processors has staked in order to have the right to process the transaction.
     */
@@ -62,7 +62,7 @@ contract PaymentsRegistry is Authorizable {
     }
 
     /** @dev Process the payment
-      * @param _subscriptionIdentifier is the identifier of that customer's subscription with its relevant details.
+      * @param _subscriptionIdentifier is the identifier of that customer's subscription with it's relevant details.
       * @param _dueDate is the date for the next billing cycle. This is to set by the executor.
       * @param _stake is how many 8x tokens are required to process.
     */
@@ -81,7 +81,7 @@ contract PaymentsRegistry is Authorizable {
     }
 
     /** @dev Allows a claimant to cancel their responsibility to process a transaction
-      * @param _subscriptionIdentifier is the identifier of that customer's subscription with its relevant details.
+      * @param _subscriptionIdentifier is the identifier of that customer's subscription with it's relevant details.
     */
     function removeClaimant(bytes32 _subscriptionIdentifier)
         public
@@ -94,7 +94,7 @@ contract PaymentsRegistry is Authorizable {
     }
 
     /** @dev A payment was cancelled by the business or user
-      * @param _subscriptionIdentifier is the identifier of that customer's subscription with its relevant details.
+      * @param _subscriptionIdentifier is the identifier of that customer's subscription with it's relevant details.
     */
     function cancelPayment(bytes32 _subscriptionIdentifier)
         public
