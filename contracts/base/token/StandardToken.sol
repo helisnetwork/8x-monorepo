7f8ed5c8e905983db3f538ea6e89a4bebf12a6b8
Zeming Yu <zemingyu@gmail.com>
2018-06-17 23:16:38 +1000

    renamed total supply

--- contracts/base/token/StandardToken.sol
+++ contracts/base/token/StandardToken.sol
@@ -16,7 +16,7 @@ contract StandardToken is ERC20 {
 
     mapping(address => uint256) balances;
 
-    uint256 totalSupply_;
+    uint256 totalTokenSupply;
 
     mapping (address => mapping (address => uint256)) internal allowed;
 
@@ -24,7 +24,7 @@ contract StandardToken is ERC20 {
     * @dev total number of tokens in existence
     */
     function totalSupply() public view returns (uint256) {
-        return totalSupply_;
+        return totalTokenSupply;
     }
 
     /**
