61053c6c02ead840afeb7082ca76c84411f4ae39
Kerman <kermankohli@gmail.com>
2018-07-13 16:07:30 +1200

    Added some basic properties and variables to the executor

--- contracts/Executor.sol
+++ contracts/Executor.sol
@@ -1,8 +1,11 @@
 pragma solidity 0.4.24;
 
 import "./base/ownership/Ownable.sol";
+
 import "./Collectable.sol";
 import "./TransferProxy.sol";
+import "./StakeContract.sol";
+import "./PaymentRegistry.sol";
 
 /** @title Contains all the data required for a user's active subscription. */
 /** @author Kerman Kohli - <kerman@TBD.com> */
@@ -10,15 +13,73 @@ import "./TransferProxy.sol";
 contract Executor is Ownable {
 
     TransferProxy public transferProxy;
+    StakeContract public stakeContract;
+    PaymentRegistry public paymentRegistry;
+
+    mapping (address => bool) public approvedContractMapping;
+    mapping (address => bool) public approvedTokenMapping;
+
+    address[] public approvedContractArray;
+    address[] public approvedTokenArray;
+
+    event LogAuthorizedContractAdded(address indexed target);
+    event LogAuthorizedContractRemoved(address indexed target);
+    event LogAuthorizedTokenAdded(address indexed target);
+    event LogAuthorizedTokenRemoved(address indexed target);
 
     /**
       * PUBLIC FUNCTIONS
     */
-    function setTransferProxy(address _proxyAddress)
+    /** @dev Set the addresses for the relevant contracts
+      * @param _transferProxyAddress the address for the designated transfer proxy.
+      * @param _stakeContractAddress the address for the stake contract.
+      * @param _paymentRegistryAddress the address for the payment registry.
+    */
+    constructor(
+        address _transferProxyAddress,
+        address _stakeContractAddress,
+        address _paymentRegistryAddress
+    )
         public
         onlyOwner
     {
-        transferProxy = TransferProxy(_proxyAddress);
+        // @TODO: Implementation
+    }
+
+    /**
+      *
+    */
+    function addApprovedContract(address _contractAddress)
+        public
+    {
+        // @TODO: Implementation
+    }
+
+    /**
+      *
+    */
+    function addApprovedToken(address _tokenAddress)
+        public
+    {
+        // @TODO: Implementation
+    }
+
+    /**
+      *
+    */
+    function removeApprovedContract(address _contractAddress)
+        public
+    {
+        // @TODO: Implementation
+    }
+
+    /**
+      *
+    */
+    function removeApprovedToken(address _tokenAddress)
+        public
+    {
+        // @TODO: Implementation
     }
 
     /** @dev Collect the payment due from the subscriber.
