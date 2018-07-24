a1dc1fb750f812dde04c2e486dcbda9f22e5e77f
Kerman <kermankohli@gmail.com>
2018-07-24 12:18:04 +1000

    Added KyberNetwork interface

--- contracts/KyberNetworkProxyInterface.sol
+++ contracts/KyberNetworkProxyInterface.sol
@@ -24,44 +24,47 @@ interface KyberNetworkProxyInterface {
         view
         returns (uint expectedRate, uint slippageRate);
 
-    function maxGasPrice() public view returns(uint);
+    function maxGasPrice()
+        public
+        view
+        returns(uint);
 
-    function getUserCapInWei(address user) public view returns(uint);
+    function getUserCapInWei(address user)
+        public
+        view
+        returns(uint);
 
-    function getUserCapInTokenWei(address user, ERC20 token) public view returns(uint);
+    function getUserCapInTokenWei(address user, ERC20 token)
+        public
+        view
+        returns(uint);
 
-    function enabled() public view returns(bool);
+    function enabled()
+        public
+        view
+        returns(bool);
 
-    function info(bytes32 id) public view returns(uint);
+    function info(bytes32 id)
+        public
+        view
+        returns(uint);
 
     /**
       * PUBLIC FUNCTIONS
     */
 
-    /** @notice use token address ETH_TOKEN_ADDRESS for ether
-      * @dev makes a trade between src and dest token and send dest token to destAddress
-      * @param src Src token
-      * @param srcAmount amount of src tokens
-      * @param dest Destination token
-      * @param destAddress Address to send tokens to
-      * @param maxDestAmount A limit on the amount of dest tokens
-      * @param minConversionRate The minimal conversion rate. If actual rate is lower, trade is canceled.
-      * @param walletId is the wallet ID to send part of the fees
-      * @param hint will give hints for the trade.
-      * @return amount of actual dest tokens
+    /** @dev Swap one token with another token
+      * @param srcToken valid token address
+      * @param srcQty in token wei
+      * @param destToken valid token address
+      * @param destAddress where to send swapped tokens to
     */
-    function tradeWithHint(
-        ERC20 src,
-        uint srcAmount,
-        ERC20 dest,
-        address destAddress,
-        uint maxDestAmount,
-        uint minConversionRate,
-        address walletId,
-        bytes hint
+    function swapTokenToToken (
+        ERC20 srcToken,
+        uint srcQty,
+        ERC20 destToken,
+        address destAddress
     )
-        public
-        payable
-        returns(uint);
+        public;
 
 }
