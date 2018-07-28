ab5843ab7553b2c83a7cc746a7f5fa48673aeaa7
Kerman <kermankohli@gmail.com>
2018-07-29 05:15:29 +1000

    Refactor Executor approved functionality into ApprovedRegistry

--- contracts/Executor.sol
+++ contracts/Executor.sol
@@ -9,79 +9,22 @@ import "./TransferProxy.sol";
 import "./StakeContract.sol";
 import "./PaymentRegistry.sol";
 import "./KyberNetworkInterface.sol";
+import "./ApprovedRegistry.sol";
 
 /** @title Contains all the data required for a user's active subscription. */
 /** @author Kerman Kohli - <kerman@8xprotocol.com> */
 
-contract Executor is Ownable {
-
-    struct GasCost {
-        uint callValue;
-        uint gasCost;
-        uint gasPrice;
-    }
+contract Executor is Ownable, ApprovedRegistry {
 
     TransferProxy public transferProxy;
     StakeContract public stakeContract;
     PaymentRegistry public paymentRegistry;
     KyberNetworkInterface public kyberProxy;
 
-    mapping (address => uint) public approvedTokenMapping;
-    mapping (address => mapping (uint => GasCost)) public approvedContractMapping;
-
-    address[] public approvedContractArray;
-    address[] public approvedTokenArray;
-
     uint public cancellationPeriod;
-
-    event LogAuthorizedContractAdded(address indexed target);
-    event LogAuthorizedContractRemoved(address indexed target);
-    event LogAuthorizedTokenAdded(address indexed target);
-    event LogAuthorizedTokenRemoved(address indexed target);
-    event LogAuthorizedTokenMultiplierSet(address indexed tokenAddress, uint indexed multiplier);
 
     event SubscriptionActivated(address subscriptionAddress, bytes32 subscriptionIdentifer);
     event SubscriptionProcessesed(address subscriptionAddress, bytes32 subscriptionIdentifer);
-
-    event ContractGasCostSet(address indexed contractAddress, uint indexed index);
-    event ContractGasCostRemoved(address indexed contractAddress, uint indexed index);
-
-    /**
-      * MODIFIERS
-    */
-
-    modifier isValidSubscriptionContract(address _subscriptionContract) {
-        require(approvedContractMapping[_subscriptionContract][0].callValue > 0);
-        _;
-    }
-
-    modifier hasContractBeenApproved(address _contractAddress, bool _expectedResult) {
-        bool contractFoundInRegistry = false;
-
-        for (uint i = 0; i < approvedContractArray.length; i++) {
-            if (approvedContractArray[i] == _contractAddress) {
-                contractFoundInRegistry = true;
-                break;
-            }
-        }
-
-        require(contractFoundInRegistry == _expectedResult);
-        _;
-    }
-
-    modifier hasTokenBeenApproved(address _tokenAddress, bool _expectedResult) {
-        bool tokenFoundInRegistry = false;
-
-        for (uint i = 0; i < approvedTokenArray.length; i++) {
-            if (approvedTokenArray[i] == _tokenAddress) {
-                tokenFoundInRegistry = true;
-                break;
-            }
-        }
-
-        require(tokenFoundInRegistry == _expectedResult);
-        _;
-    }
 
     /**
       * PUBLIC FUNCTIONS
@@ -114,151 +57,6 @@ contract Executor is Ownable {
     */
     function setCancellationPeriod(uint _period) public onlyOwner {
         cancellationPeriod = _period;
-    }
-
-    /** @dev Add an approved subscription contract to be used.
-      * @param _contractAddress is the address of the subscription contract.
-    */
-    function addApprovedContract(address _contractAddress)
-        public
-        onlyOwner
-        hasContractBeenApproved(_contractAddress, false)
-    {
-        approvedContractArray.push(_contractAddress);
-        emit LogAuthorizedContractAdded(_contractAddress);
-    }
-
-    /** @dev Set an approved contract call cost.
-      * @param _contractAddress is the address of the subscription contract.
-      * @param _index is the reference to the call (cancel, subscribe etc).
-      * @param _callValue is how much the transaction will cost.
-      * @param _gasCost is the amount of gas that will be used.
-      * @param _gasPrice is the gas price that will be reimbursed up to.
-    */
-    function setApprovedContractCallCost(
-        address _contractAddress,
-        uint _index,
-        uint _callValue,
-        uint _gasCost,
-        uint _gasPrice
-    )
-        public
-        onlyOwner
-        hasContractBeenApproved(_contractAddress, true)
-    {
-        approvedContractMapping[_contractAddress][_index] = GasCost({
-            callValue: _callValue,
-            gasCost: _gasCost,
-            gasPrice: _gasPrice
-        });
-
-        emit ContractGasCostSet(_contractAddress, _index);
-
-    }
-
-    /** @dev Add an approved token to be used.
-      * @param _tokenAddress is the address of the token to be used.
-    */
-    function addApprovedToken(address _tokenAddress)
-        public
-        onlyOwner
-        hasTokenBeenApproved(_tokenAddress, false)
-    {
-        approvedTokenArray.push(_tokenAddress);
-        emit LogAuthorizedTokenAdded(_tokenAddress);
-    }
-
-    /** @dev Set an approved token multiplier.
-      * @param _tokenAddress is the address of the token to be used.
-      * @param _multiplier is the amount of this token required for each subscription.
-    */
-    function setApprovedTokenMultiplier(
-        address _tokenAddress,
-        uint _multiplier
-    )
-        public
-        onlyOwner
-        hasTokenBeenApproved(_tokenAddress, true)
-    {
-        approvedTokenMapping[_tokenAddress] = _multiplier;
-        emit LogAuthorizedTokenMultiplierSet(_tokenAddress, _multiplier);
-
-    }
-
-    /** @dev Remove an approved subscription contract.
-      * @param _contractAddress is the address of the subscription contract.
-    */
-    function removeApprovedContract(address _contractAddress)
-        public
-        onlyOwner
-    {
-        for (uint i = 0; i < approvedContractArray.length; i++) {
-            if (approvedContractArray[i] == _contractAddress) {
-                approvedContractArray[i] = approvedContractArray[approvedContractArray.length - 1];
-                approvedContractArray.length--;
-
-                emit LogAuthorizedContractRemoved(_contractAddress);
-
-                break;
-            }
-        }
-    }
-
-    /** @dev Remove an approved contract call cost.
-      * @param _contractAddress is the address of the contract.
-      * @param _index is the reference to the call (cancel, subscribe etc).
-    */
-    function removeApprovedContractCallCost(
-        address _contractAddress,
-        uint _index
-    )
-        public
-        onlyOwner
-    {
-        delete approvedContractMapping[_contractAddress][_index];
-
-        emit ContractGasCostRemoved(_contractAddress, _index);
-    }
-
-    /** @dev Remove an approved token to be used.
-      * @param _tokenAddress is the address of the token to remove.
-    */
-    function removeApprovedToken(address _tokenAddress)
-        public
-        onlyOwner
-    {
-        for (uint i = 0; i < approvedTokenArray.length; i++) {
-            if (approvedTokenArray[i] == _tokenAddress) {
-                approvedTokenArray[i] = approvedTokenArray[approvedTokenArray.length - 1];
-                approvedTokenArray.length--;
-
-                delete approvedTokenMapping[_tokenAddress];
-
-                emit LogAuthorizedTokenRemoved(_tokenAddress);
-
-                break;
-            }
-        }
-    }
-
-    /** @dev Get approved contract array.
-    */
-    function getApprovedContracts()
-        public
-        constant
-        returns (address[])
-    {
-        return approvedContractArray;
-    }
-
-    /** @dev Get approved token array.
-    */
-    function getApprovedTokens()
-        public
-        constant
-        returns (address[])
-    {
-        return approvedTokenArray;
     }
 
     /** @dev Active a subscription once it's been created (make the first payment) paid from wrapped Ether.
