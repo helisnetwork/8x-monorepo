8994fa17bcac2872058262a96103c5352dd225e6
Andy Chen Fang <andy@8xprotocol.com>
2018-08-10 16:59:32 +1000

    Put content for options interface

--- packages/payment-gateway/src/pages/select-wallet.js
+++ packages/payment-gateway/src/pages/select-wallet.js
@@ -1,11 +1,47 @@
 /* Import statements */
 import React from 'react';
 
+import {default as Images} from '../middleware/images';
+
 /* App component */
 class SelectWallet extends React.Component {
   render() {
     return (
-      <p>Hello World</p>
+      <div className="main-card">
+        <div className="card-header">
+          <p>Where are your funds stored?</p>
+          <p>Please select an option</p>
+        </div>
+        <div className="option-container">
+          <div className="metamask-container">
+            <div className="metamaskLogo">
+              <img src={Images.metamaskLogo}/>
+            </div>
+            <div className="metamaskText">
+              <p>Metamask</p>
+              <p>MetaMask is a browser extension that allows you to store Ether and interact with decentralised apps.</p>
+            </div>
+          </div>
+          <div className="ledger-container">
+            <div className="ledgerLogo">
+              <img src={Images.ledgerLogo}/>
+            </div>
+            <div className="ledger-text">
+              <p>Ledger</p>
+              <p>You store your cryptocurrency on a Ledger Hardware Wallet.</p>
+            </div>
+          </div>
+          <div className="trezor-container">
+            <div className="trezorLogo">
+              <img src={Images.trezorLogo}/>
+            </div>
+            <div className="trezor-text">
+              <p>Trezor</p>
+              <p>You store your cryptocurrency on a Trezor Hardware Wallet.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 };
