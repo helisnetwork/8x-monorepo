95b1edb8737804aefa299fa7d2468550f2cd3cde
Andy Chen Fang <andy@8xprotocol.com>
2018-08-10 16:59:32 +1000

    Put content for options interface

--- packages/payment-gateway/src/middleware/images.js
+++ packages/payment-gateway/src/middleware/images.js
@@ -1,5 +1,10 @@
 // import abacusGreyLogo from '../assets/images/abacus-grey-logo.png';
+import metamaskLogo from '../assets/images/metamask.png';
+import ledgerLogo from '../assets/images/ledger.png';
+import trezorLogo from '../assets/images/trezor.png';
 
 export default {
-  
+  metamaskLogo,
+  ledgerLogo,
+  trezorLogo
 };
