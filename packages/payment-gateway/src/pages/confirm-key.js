9aa0b7d1a1529e33b51b9767a03bdee7ca65ab93
Andy Chen Fang <andy@8xprotocol.com>
2018-08-13 15:43:35 +1000

    Added styling for confirm key screen

--- packages/payment-gateway/src/pages/confirm-key.js
+++ packages/payment-gateway/src/pages/confirm-key.js
@@ -10,29 +10,27 @@ class ConfirmKey extends React.Component {
     return (
       <div>
         <div className="small-card">
-          <div className="main-card">
-            <Header title="Confirm your Keys"/>
-            <div className="hero">
-              <div className="text">
-                <h1>Please type in your first, third and last key.</h1>
-                <p className="middle-text">We need to make sure you have written down your keys.</p>
+          <Header title="Confirm your Keys"/>
+          <div className="hero">
+            <div className="text">
+              <h1>Please type in your first, third and last key.</h1>
+              <p className="middle-text">We need to make sure you have written down your keys.</p>
+            </div>
+            <div className="confirm-box">
+              <div className="item">
+                <p>First Key</p>
               </div>
-              <div className="confirm-box">
-                <div className="item">
-                  <p>First Key</p>
-                </div>
-                <div className="item">
-                  <p>Third Key</p>
-                </div>
-                <div className="item">
-                  <p>Last Key</p>
-                </div>
+              <div className="item">
+                <p>Third Key</p>
+              </div>
+              <div className="item">
+                <p>Last Key</p>
               </div>
             </div>
-            <div className="button">
-              <p>Please type in your keys</p>
-            </div>
-          </div> 
+          </div>
+          <div className="cta-button">
+            <p>Please type in your keys</p>
+          </div>
         </div>    
       </div>
     );
