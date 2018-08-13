391df3adcec31a040a9fe143213eb851ac83e444
Andy Chen Fang <andy@8xprotocol.com>
2018-08-13 15:27:55 +1000

    Added basic html for confirm key screen

---
+++ packages/payment-gateway/src/pages/confirm-key.js
@@ -0,0 +1,43 @@
+/* Import statements */
+import React from 'react';
+
+import Header from '../components/header.js';
+import {default as Images} from '../middleware/images';
+
+/* App component */
+class ConfirmKey extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="small-card">
+          <div className="main-card">
+            <Header title="Confirm your Keys"/>
+            <div className="hero">
+              <div className="text">
+                <h1>Please type in your first, third and last key.</h1>
+                <p className="middle-text">We need to make sure you have written down your keys.</p>
+              </div>
+              <div className="confirm-box">
+                <div className="item">
+                  <p>First Key</p>
+                </div>
+                <div className="item">
+                  <p>Third Key</p>
+                </div>
+                <div className="item">
+                  <p>Last Key</p>
+                </div>
+              </div>
+            </div>
+            <div className="button">
+              <p>Please type in your keys</p>
+            </div>
+          </div> 
+        </div>    
+      </div>
+    );
+  }
+};
+
+
+export default ConfirmKey;
