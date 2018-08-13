563db979363c98c90198073328cd17afee306359
Andy Chen Fang <andy@8xprotocol.com>
2018-08-13 15:03:31 +1000

    Added styling to private key screen (not responsive)

--- packages/payment-gateway/src/pages/privatekey.js
+++ packages/payment-gateway/src/pages/privatekey.js
@@ -11,17 +11,39 @@ class Privatekey extends React.Component {
       <div>
         <div className="small-card">
           <Header title="Save your Private Keys"/>
-          <div className="header">
-            <h1>Please carefully write down your keys, or <span>print</span> them.</h1>
-          </div>
-          <div className="item">
-          </div>
-          <div className="middle-text">
-            <p>Once you have finished writing down your keys, please store them in a safe location. It is a good idea to make copies, and store your keys across multiple locations. Please DO NOT store your keys on your computer, or an online service such as Dropbox of Google Drive.</p>
-          </div>
-          <div className="checkbox">
-            <p>Checkbox</p>
-            <p>Checkbox2</p>
+          <div className="hero">
+            <div className="header-text">
+              <h1>Please carefully write down your keys, or <span>print</span> them.</h1>
+            </div>
+            <div className="item">
+              <p>Dog</p> 
+              <p>Cheese</p>
+              <p>Seed</p>
+              <p>Cat</p>
+              <p>Excited</p>
+              <p>Bear</p>
+              <p>Mouse</p>
+              <p>Happy</p>
+              <p>Please</p>
+              <p>Pig</p>
+              <p>Hour</p>
+              <p>Hello</p>
+            </div>
+            <div className="middle-text">
+              <p>Once you have finished writing down your keys, please store them in a safe location. It is a good idea to make copies, and store your keys across multiple locations. Please DO NOT store your keys on your computer, or an online service such as Dropbox of Google Drive.</p>
+            </div>
+            <div className="checkbox">
+              <div className="tick one">
+                <div className="square">
+                </div>
+                <p>I have printed or written down my keys</p>
+              </div>
+              <div className="tick two">
+                <div className="square">
+                </div>
+                <p>I have stored my keys in safe place</p>
+              </div>
+            </div>
           </div>
           <div className="cta-button">
             <p className="text">Please confirm above</p>
