6f209ba483ada362b0e7d7554789feb214e50314
Andy Chen Fang <andy@8xprotocol.com>
2018-08-14 14:58:10 +1000

    Created component for drop down

---
+++ packages/payment-gateway/src/components/dropdown.js
@@ -0,0 +1,77 @@
+/* Import statements */
+import React from 'react';
+
+import Header from '../components/header.js';
+import {default as Images} from '../middleware/images';
+
+/* App component */
+
+class Dropdown extends React.Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      showMenu: false,
+    };
+    
+    this.showMenu = this.showMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+  }
+  
+  showMenu(event) {
+    event.preventDefault();
+    
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener('click', this.closeMenu);
+    });
+  }
+  
+  closeMenu(event) {
+    
+    if (!this.dropdownMenu.contains(event.target)) {
+      
+      this.setState({ showMenu: false }, () => {
+        document.removeEventListener('click', this.closeMenu);
+      });  
+      
+    }
+  }
+
+  render() {
+    return (
+      <div className="dropdown-container">
+        <button className="button" onClick={this.showMenu}>
+          <div className="dropdown">
+            <div className="label">
+              <img src={Images.ethLogo}/>
+              <p>Ethereum</p>
+              <p>ETH</p>
+              <p className="triangle"></p>
+            </div>
+          </div>
+        </button>
+        
+        {
+          this.state.showMenu
+            ? (
+              <div
+                className="menu"
+                ref={(element) => {
+                  this.dropdownMenu = element;
+                }}
+              >
+                <button> Menu item 1 </button>
+                <button> Menu item 2 </button>
+                <button> Menu item 3 </button>
+              </div>
+            )
+            : (
+              null
+            )
+        }
+      </div>
+    );
+  }
+}
+
+export default Dropdown;
