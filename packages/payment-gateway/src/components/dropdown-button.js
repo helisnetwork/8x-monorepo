52b781f44e4727d390a291e6903666a34d40aede
Andy Chen Fang <andy@8xprotocol.com>
2018-08-17 14:21:34 +1000

    Updated react component for drop down

--- packages/payment-gateway/src/components/dropdown-button.js
+++ packages/payment-gateway/src/components/dropdown-button.js
@@ -7,14 +7,15 @@ class DropdownButton extends React.Component {
   render() {
     return (
       <div>
-        <button className="button">
+        <button className="button" onClick={this.props.action}>
           <div className="label">
             <div className="left">
-              <img className="logo" src={this.props.images}/>
-              <p className="coin-name">{this.props.name}</p>
+              <img className="logo" src={this.props.image}/>
+              <p className="coin-name">{this.props.item.name}</p>
             </div>
             <div className="right">
               <p className="coin-ticker">{this.props.ticker}</p>
+              {this.props.showTriangle ? <p className="triangle">{this.props.triangle}</p> : null}
             </div>
           </div>
         </button>
