42b33ac51af8fbd464df0edf686894df64696d54
Kerman <kermankohli@gmail.com>
2018-08-17 14:41:35 +1000

    Changed multiplier to requirements

--- test/requirements.js
+++ test/requirements.js
@@ -1,23 +1,23 @@
 import assertRevert from './helpers/assert_revert.js';
 
-var Multiplier = artifacts.require('./Multiplier.sol');
+var Requirements = artifacts.require('./Requirements.sol');
 
-contract('Multiplier', function(accounts) {
+contract('Requirements', function(accounts) {
 
-    let multiplierContract;
+    let requirementsContract;
     let gini = 400;
     let divideTotalBy = 15;
 
     before(async function() {
 
-        multiplierContract = await Multiplier.new(gini, divideTotalBy, {from: accounts[0]});
+        requirementsContract = await Requirements.new(gini, divideTotalBy, {from: accounts[0]});
 
     });
 
     it("should be able to iniate the contract with the correct variables", async function() {
 
-        let returnedGini = await multiplierContract.giniCoefficient.call();
-        let returnedDivideTotalBy = await multiplierContract.divideTotalBy.call();
+        let returnedGini = await requirementsContract.giniCoefficient.call();
+        let returnedDivideTotalBy = await requirementsContract.divideTotalBy.call();
 
         assert.equal(returnedGini, gini);
         assert.equal(returnedDivideTotalBy, divideTotalBy);
@@ -26,34 +26,34 @@ contract('Multiplier', function(accounts) {
 
     it("should not be able to set the gini coefficient as someone other than the owner", async function() {
 
-        await assertRevert(multiplierContract.setGiniCoefficient(0.4, {from: accounts[1]}));
+        await assertRevert(requirementsContract.setGiniCoefficient(0.4, {from: accounts[1]}));
 
     });
 
     it("should not be able to set the total to divide by as someone other than the owner", async function() {
 
-        await assertRevert(multiplierContract.setDivideTotalBy(0.9, {from: accounts[1]}));
+        await assertRevert(requirementsContract.setDivideTotalBy(0.9, {from: accounts[1]}));
 
     });
 
     it("should be able to set the gini coefficient as the ownner", async function() {
 
         gini = 500;
-        await multiplierContract.setGiniCoefficient(gini, {from: accounts[0]});
+        await requirementsContract.setGiniCoefficient(gini, {from: accounts[0]});
 
     });
 
     it("should be able to set the total to divide by as the ownner", async function() {
 
         divideTotalBy = 10;
-        await multiplierContract.setDivideTotalBy(divideTotalBy, {from: accounts[0]});
+        await requirementsContract.setDivideTotalBy(divideTotalBy, {from: accounts[0]});
 
     });
 
     it("should be able to return the correct valid stake after 1 day", async function() {
 
         let now = (Date.now() / 1000);
-        let stake = await multiplierContract.getMultiplier(
+        let stake = await requirementsContract.getMultiplier(
             now,
             now + (60 * 60 * 24),
             now + (60 * 60 * 24 * 5),
