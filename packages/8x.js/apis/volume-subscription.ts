0e277196f88e7f2cd2f878cbd35b0f8add9ee098
Kerman <kermankohli@gmail.com>
2018-08-31 15:15:03 +1000

    Types assigned to executor + volume subscription functions

--- packages/8x.js/apis/volume-subscription.ts
+++ packages/8x.js/apis/volume-subscription.ts
@@ -1,30 +1,73 @@
+import * as Types from "../types/common";
+
 export default class VolumeSubscription {
 
-  public async createPlan() {
+  public async createPlan(
+    owner: Types.Address,
+    tokenAddress: Types.Address,
+    identifier: string,
+    interval: Types.UInt,
+    amount: Types.UInt,
+    fee: Types.UInt,
+    data: string
+  ): Promise<Types.Bytes32> {
+
+    // @TODO: Implementation
+    return Promise.resolve('todo');
 
   }
 
-  public async updatePlanOwner() {
+  public async updatePlanOwner(
+    plan: Types.Bytes32,
+    owner: Types.Address
+  ): Promise<any> {
+
+    // @TODO: Implementation
 
   }
 
-  public async setPlanData() {
+  public async setPlanData(
+    plan: Types.Bytes32,
+    data: string
+  ): Promise<any> {
+
+    // @TODO: Implementation
 
   }
 
-  public async terminatePlan() {
+  public async terminatePlan(
+    plan: Types.Bytes32,
+    terminationDate: Types.UInt
+  ): Promise<any> {
+
+    // @TODO: Implementation
 
   }
 
-  public async createSubscription() {
+  public async createSubscription(
+    planHash: Types.Bytes32,
+    data: string
+  ): Promise<Types.Bytes32> {
+
+    // @TODO: Implementation
+    return Promise.resolve('todo');
 
   }
 
-  public async setSubscriptionData() {
+  public async setSubscriptionData(
+    subscription: Types.Bytes32,
+    data: string
+  ): Promise<any> {
+
+    // @TODO: Implementation
 
   }
 
-  public async cancelSubscription() {
+  public async cancelSubscription(
+    subscription: Types.Bytes32
+  ) {
+
+    // @TODO: Implementation
 
   }
 
