34512767e4f23de4c1482a00f550baf720d35ca6
Kerman <kermankohli@gmail.com>
2018-08-31 10:12:24 +1000

    Added stake contract stubs

---
+++ packages/8x.js/apis/stake-contract.ts
@@ -0,0 +1,39 @@
+export default class StakeContract {
+
+  public async getTotalStake() {
+
+  }
+
+  public async getAvailableStake() {
+
+  }
+
+  public async getLockedStake() {
+
+  }
+
+  public async getTotalTokenStake() {
+
+  }
+
+  public async getAvailableTokenStake() {
+
+  }
+
+  public async getLockedTokenStake() {
+
+  }
+
+  public async getTokenStakeDetails() {
+
+  }
+
+  public async topUpStake() {
+
+  }
+
+  public async withdrawStake() {
+
+  }
+
+}
