bb425092a9540904ff13b3d40656cfd64289b9d7
Kerman <kermankohli@gmail.com>
2018-09-04 15:39:32 +1000

    Updated deployment script + export addresses to deployed artifacts

--- packages/contracts/migrations/2_deploy_contracts.js
+++ packages/contracts/migrations/2_deploy_contracts.js
@@ -1,149 +1,92 @@
-var TransferProxy = artifacts.require("./TransferProxy.sol");
-var ApprovedRegistry = artifacts.require("./ApprovedRegistry.sol");
-var PaymentRegistry = artifacts.require("./PaymentRegistry.sol");
-var StakeContract = artifacts.require("./StakeContract.sol");
-var Requirements = artifacts.require("./Requirements.sol");
-var Executor = artifacts.require("./Executor.sol");
+const fs = require('fs-extra');
 
-var EightExToken = artifacts.require("./EightExToken.sol");
-var WrappedEther = artifacts.require("./base/token/WETH.sol");
+const Tokens = require("../../artifacts/deployed/tokens");
+const Dependencies = require("../../artifacts/deployed/dependencies");
 
-var VolumeSubscription = artifacts.require("./VolumeSubscription.sol");
+const Constants = require("./migration_constants");
 
 module.exports = function(deployer, network, accounts) {
 
-    console.log(`Using: ${network}`);
+    const TransferProxy = artifacts.require("./TransferProxy.sol");
+    const ApprovedRegistry = artifacts.require("./ApprovedRegistry.sol");
+    const PaymentRegistry = artifacts.require("./PaymentRegistry.sol");
+    const StakeContract = artifacts.require("./StakeContract.sol");
+    const Requirements = artifacts.require("./Requirements.sol");
+    const VolumeSubscription = artifacts.require("./VolumeSubscription.sol");
+    const Executor = artifacts.require("./Executor.sol");
+    const EightExToken = artifacts.require("./EightExToken.sol");
 
-    if (network == 'development') {
-        return;
-    }
+    const MockToken = artifacts.require("./test/MockToken.sol");
+    const MockKyberNetworkInterface = artifacts.require("./test/MockKyberNetworkInterface.sol");
 
-    let transferProxy;
-    let stakeContract;
-    let paymentRegistry;
-    let approvedRegistry;
-    let executor;
-    let requirementsContract;
+    return deployer.then(async () => {
 
-    let eightExToken;
-    let wrappedEther;
+        let executor;
+        let volumeSubscription;
+        let approvedRegistry;
 
-    let volumeSubscription;
+        let transferProxy = await deployer.deploy(TransferProxy);
+        let paymentRegistry = await deployer.deploy(PaymentRegistry);
+        let requirementsContract = await deployer.deploy(Requirements);
 
-    let daiAddress = (network == 'live') ? '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359' : '0xc4375b7de8af5a38a93548eb8453a498222c4ff2';
-    let kyberNetwork = (network == 'live') ? '0x818E6FECD516Ecc3849DAf6845e3EC868087B755' : '0x7e6b8b9510D71BF8EF0f893902EbB9C865eEF4Df';
+        let eightExToken = await deployer.deploy(EightExToken);
+        let stakeContract = await deployer.deploy(StakeContract, eightExToken.address);
 
-    /**
-     * Deploy a transfer proxy
-     * Deploy the 8x token
-     * Deploy the stake contract with the 8x token in the initialiser
-     * Deploy the approved registry
-     * Add the volume subscription as an approved contract to the registry
-     * Deploy WETH
-     * Add WETH + DAi as approved tokens to registry
-     * Deploy the volume subscription contract with approve registry in constructor
-     * Deploy the executor with transfer proxy, stake contract, payment registry & approved registry
-     * Add the executor as an authorised contract to transfer proxy, stake contract, volume subscription & payment registry
-    */
+        let kyberNetworkAddress = Dependencies.KyberNetwork[network] || (await deployer.deploy(MockKyberNetworkInterface)).address;
+        let daiAddress = Tokens.DAI.addresses[network] || (await deployer.deploy(MockToken)).address;
 
-    deployer.deploy(TransferProxy).then(function(instance) {
-        transferProxy = instance;
-        return deployer.deploy(EightExToken);
-    }).then(function(instance) {
-        eightExToken = instance;
-        return deployer.deploy(StakeContract, eightExToken.address);
-    }).then(function(instance) {
-        stakeContract = instance;
-        return deployer.deploy(ApprovedRegistry, kyberNetwork)
-    }).then(function(instance) {
-        approvedRegistry = instance;
-        return deployer.deploy(VolumeSubscription, approvedRegistry.address);
-    }).then(function(instance) {
-        volumeSubscription = instance;
-        return deployer.deploy(WrappedEther);
-    }).then(function(instance) {
-        wrappedEther = instance;
-        return deployer.deploy(PaymentRegistry)
-    }).then(function(instance) {
-        paymentRegistry = instance;
-        return deployer.deploy(Requirements);
-    }).then(function(instance) {
-        requirementsContract = instance;
-        return Promise.all[
-            approvedRegistry.addApprovedContract(volumeSubscription.address),
-            approvedRegistry.addApprovedToken(wrappedEther.address, true),
-            approvedRegistry.addApprovedToken(daiAddress, false)
-        ];
-    }).then(function(instance) {
-        return deployer.deploy(
+        // Deploy the Approved Registrt with Kyber Network
+        approvedRegistry = await deployer.deploy(ApprovedRegistry, kyberNetworkAddress);
+
+        // Deploy the Volume Subscription contract with the Approved Registry
+        volumeSubscription = await deployer.deploy(VolumeSubscription, approvedRegistry.address);
+
+        // Add Dai to the approved token token registry
+        await approvedRegistry.addApprovedToken(daiAddress, false)
+
+        // Add Volume Subscription as an approved contract
+        await approvedRegistry.addApprovedContract(volumeSubscription.address);
+
+        // Deploy the executor contract
+        executor = await deployer.deploy(
             Executor,
             transferProxy.address,
             stakeContract.address,
             paymentRegistry.address,
             approvedRegistry.address,
             requirementsContract.address,
-            800,
-            7
-        )
-    }).then(function(instance) {
-        executor = instance;
-        return Promise.all[
-            transferProxy.addAuthorizedAddress(executor.address),
-            stakeContract.addAuthorizedAddress(executor.address),
-            paymentRegistry.addAuthorizedAddress(executor.address),
-            volumeSubscription.addAuthorizedAddress(executor.address)
-        ];
-    }).then(function(instance) {
-        if (network == 'staging') {
-            let subscriptionIdentifier;
-            let planIdentifier;
+            Constants.LOCK_UP_PERCENTAGE,
+            Constants.MAXIMUM_INTERVAL_DIVISOR
+        );
 
-            let owner = accounts[0];
-            let business = accounts[1];
-            let user = accounts[2];
-            let serviceNode = accounts[3];
+        // Export JSON
+        let output = {
+            network: network,
+            addresses: {
+                'Executor': executor.address,
+                'VolumeSubscription': volumeSubscription.address,
+                'ApprovedRegistry': approvedRegistry.address,
+                'TransferProxy': transferProxy.address,
+                'PaymentRegistry': paymentRegistry.address,
+                'RequirementsContract': requirementsContract.address,
+                'EightExToken': eightExToken.address,
+                'StakeContract': stakeContract.address,
+                'KyberNetwork': kyberNetworkAddress,
+                'DAI': daiAddress
+            },
+            approvedTokens: [
+                daiAddress
+            ],
+            approvedContracts: {
+                'VolumeSubscription': {
+                    address: volumeSubscription.address
+                }
+            },
+            lockUpPercentage: Constants.LOCK_UP_PERCENTAGE,
+            maximumIntervalDivisor: Constants.MAXIMUM_INTERVAL_DIVISOR,
+        };
 
-            return volumeSubscription.createPlan(
-                business,
-                wrappedEther.address,
-                "8x.new.plan",
-                10,
-                1*10**18,
-                10**17,
-                "",
-                {from: business}
-            ).then(function(result) {
-                planIdentifier = result.logs[0].args.identifier;
-                console.log(`Plan identifier is: ${planIdentifier}`);
-                return volumeSubscription.createSubscription(
-                    planIdentifier,
-                    "",
-                    {from: user}
-                );
-            }).then(function(result) {
-                subscriptionIdentifier = result.logs[0].args.identifier;
-                console.log(`Subscription identifier is: ${subscriptionIdentifier}`);
-                return wrappedEther.deposit({from: user, value: 10*10**18});
-            }).then(function(result) {
-                console.log(
-                    `Executor.at("${executor.address}").activateSubscription("${volumeSubscription.address}","${subscriptionIdentifier}", {from: "${user}"});`
-                );
-                return wrappedEther.approve(transferProxy.address, 10*10**18, {from: user});
-            }).then(function(result) {
-                return eightExToken.transfer(serviceNode, 100*10**18, {from: owner});
-            }).then(function(result) {
-                return eightExToken.approve(stakeContract.address, 100*10**18, {from: serviceNode})
-            }).then(function(result) {
-                console.log(
-                    `Executor.at("${executor.address}").processSubscription("${volumeSubscription.address}","${subscriptionIdentifier}", {from: "${serviceNode}"});`
-                )
-                return stakeContract.topUpStake(100*10**18, wrappedEther.address, {from: serviceNode});
-            });
-        }
-
-        return null;
-    }).catch(function(error) {
-        console.log("CATCH-ERROR " + error);
-    })
+        await fs.outputFile(Constants.configPath(network), JSON.stringify(output, null, 2));
+    });
 
 };
