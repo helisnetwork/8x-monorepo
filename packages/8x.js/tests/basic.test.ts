5c7c15bd1e2cdbe02ca9a4afa073b4ab390cf4d5
Kerman <kermankohli@gmail.com>
2018-09-04 16:13:06 +1000

    Able to run tests script

---
+++ packages/8x.js/tests/basic.test.ts
@@ -0,0 +1,12 @@
+import * as mocha from 'mocha';
+import * as chai from 'chai';
+
+const expect = chai.expect;
+
+describe('Plans', () => {
+
+  it('should be able to create a plan', () => {
+    expect(true).to.be.true;
+  });
+
+});
