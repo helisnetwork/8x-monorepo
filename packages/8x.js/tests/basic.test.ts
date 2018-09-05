a56f8c8ce5b192f05f54c292f28667b65dcb9491
Kerman <kermankohli@gmail.com>
2018-09-05 11:39:08 +1000

    wip

--- packages/8x.js/tests/basic.test.ts
+++ packages/8x.js/tests/basic.test.ts
@@ -1,11 +1,26 @@
 import * as mocha from 'mocha';
 import * as chai from 'chai';
 
+import { returnConfig } from 'artifacts';
+import configToJson from '../src/utils/configToJson';
+
+import Web3 from 'web3';
+
 const expect = chai.expect;
+const config = returnConfig('development')
+const addressBook = configToJson(config);
+
+console.log(addressBook);
+
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
 describe('Plans', () => {
 
-  it('should be able to create a plan', () => {
+  beforeEach(async () => {
+
+  });
+
+  it('should be able to create a plan', async () => {
     expect(true).to.be.true;
   });
 
