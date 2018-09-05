f42ec4297d7ba409f6e98320acdc30f04fb8e117
Kerman <kermankohli@gmail.com>
2018-09-05 14:30:36 +1000

    Address book included in exports

--- packages/8x.js/tests/basic.test.ts
+++ packages/8x.js/tests/basic.test.ts
@@ -1,16 +1,14 @@
 import * as mocha from 'mocha';
 import * as chai from 'chai';
 
-import { returnConfig } from 'artifacts';
-import configToJson from '../src/utils/configToJson';
-
 import Web3 from 'web3';
 
-const expect = chai.expect;
-const config = returnConfig('development')
-const addressBook = configToJson(config);
+import { getAddressBook } from 'artifacts';
 
-console.log(addressBook);
+const expect = chai.expect;
+const addressBook = getAddressBook('development');
+
+consol
 
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
