84912b9080250798c609b94460eb11815498dd7f
AndyTCF <andy.c.f.98@gmail.com>
2018-09-12 14:35:54 +1000

    Added images and updated styling

--- packages/payment-gateway/src/pages/conversion-prompt.js
+++ packages/payment-gateway/src/pages/conversion-prompt.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/header';
+import { default as Images } from '../middleware/images';
 
 class ConversionPrompt extends React.Component {
 
@@ -11,22 +12,32 @@ class ConversionPrompt extends React.Component {
     return (
       <div className="small-card">
         <Header title="Convert to Dai" previousPage="/metamask-handler"/>
-        <div className="hero">
+        <div className="main-content">
           <div className="main-text">
-            <h2>Once you begin your subscription, your Ethereum will be converted into Dai.</h2>
+            <h2 id="message">Once you begin your subscription, your Ethereum will be converted into Dai.</h2>
           </div>
           <div className="main-graphics">
-            <img/>
-            <img/>
-            <img/>
+            <div className="ethereum">
+              <img className="logo" src={Images.ethLogo}/>
+              <p>0.014</p>
+              <p>ETH</p>
+            </div>
+            <div className="arrow">
+              <img className="logo" src={Images.arrow}/>
+            </div>
+            <div className="dai">
+              <img className="logo" src={Images.daiLogo}/>
+              <p>84.00</p>
+              <p>DAI</p>
+            </div>
           </div>
           <div className="secondary-text">
-            <p>Why is this happening?</p>
+            <p className="title">Why is this happening?</p>
             <p className="paragraph">DAI is a stablecoin that is pegged to the US Dollar, this means it’s value will always stay around $1.00ea. By converting your ETH to DAI, you will avoid the price fluctuations and will have enough funds to cover your subscription for 6 months.</p>
           </div>
         </div>
         <div className="button">
-          <p>Convert to DAI</p>
+          <p className="convert">Convert to DAI</p>
         </div>
       </div>
     );
