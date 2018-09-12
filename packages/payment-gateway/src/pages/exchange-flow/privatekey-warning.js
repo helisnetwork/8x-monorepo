b808da06887ed2e87e251389b72aeadd2c0ec1e7
AndyTCF <andy.c.f.98@gmail.com>
2018-09-12 12:51:23 +1000

    Moved styling sheets into separate folder

--- packages/payment-gateway/src/pages/exchange-flow/privatekey-warning.js
+++ packages/payment-gateway/src/pages/exchange-flow/privatekey-warning.js
@@ -18,7 +18,7 @@ class PrivatekeyWarning extends React.Component {
             <div className="text">
               <h1>How do you access your wallet?</h1>
               <p className="middle-text">Just like the pin number for your bank account, your personal wallet uses a Private Key. Your private key will be given to you in the form of 12 randomly generate words, often referred to as your seed phase.</p>
-              <h2>Please note. If you lose your key, you will lose access to any funds stored in your wallet.</h2>
+              <h2 className="warning">Please note. If you lose your key, you will lose access to any funds stored in your wallet.</h2>
             </div>
           </div>
           <div className="cta-button">
