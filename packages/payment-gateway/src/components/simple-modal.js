9d7d9a67b29c883a1d7adc098141d3edeb9e6ee5
AndyTCF <andy.c.f.98@gmail.com>
2018-09-12 11:58:40 +1000

    Refactored onClick modal launcher

--- packages/payment-gateway/src/components/simple-modal.js
+++ packages/payment-gateway/src/components/simple-modal.js
@@ -1,45 +1,51 @@
 import React, { Component } from 'react';
 import isNil from 'lodash/fp/isNil';
+
+import bus from '../bus';
+
 class SimpleModal extends Component {
+
   constructor(props) {
     super(props);
     this.handleKeyUp = this.handleKeyUp.bind(this);
     this.handleOutsideClick = this.handleOutsideClick.bind(this);
+
   }
+
   componentDidMount() {
     window.addEventListener('keyup', this.handleKeyUp, false);
     document.addEventListener('click', this.handleOutsideClick, false);
   }
+
   componentWillUnmount() {
     window.removeEventListener('keyup', this.handleKeyUp, false);
     document.removeEventListener('click', this.handleOutsideClick, false);
   }
+
   handleKeyUp(e) {
-    const { onCloseRequest } = this.props;
     const keys = {
       27: () => {
         e.preventDefault();
-        onCloseRequest();
+        bus.trigger('modal:show', false);
         window.removeEventListener('keyup', this.handleKeyUp, false);
       },
     };
     if (keys[e.keyCode]) { keys[e.keyCode](); }
   }
+
   // Changed !isNil(this.modal) to allow drop down menus to work within modal
   handleOutsideClick(e) {
-    const { onCloseRequest } = this.props;
     if (isNil(this.modal)) {
       if (!this.modal.contains(e.target)) {
-        onCloseRequest();
+        bus.trigger('modal:show', false);
         document.removeEventListener('click', this.handleOutsideClick, false);
       }
     }
   }
+
   render () {
-    const {
-      onCloseRequest,
-      children,
-    } = this.props;
+    const { children } = this.props;
+
     return (
       <div className='modal-overlay'>
         <div
@@ -53,7 +59,7 @@ class SimpleModal extends Component {
         <button
           type="button"
           className='close-button'
-          onClick={onCloseRequest}
+          onClick={() => bus.trigger('modal:show', false)}
         />
       </div>
     );
