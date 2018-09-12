f629a3cdc2afc75e8ee5280df819ede12ddb1ad7
AndyTCF <andy.c.f.98@gmail.com>
2018-09-12 15:18:38 +1000

    Added basic html for begin-subscription page

---
+++ packages/payment-gateway/src/pages/begin-subscription.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Header from '../components/header';
+import { default as Images } from '../middleware/images';
+
+class BeginSubscription extends React.Component {
+
+  constructor(){
+    super();
+  }
+
+  render(){
+    return (
+      <div className="small-card">
+        <Header title="Begin Subscription" previousPage='/conversion'/>
+        <div className="hero">
+          <div className="header-text">
+            <h2 className="header-bold">Your conversion is now complete</h2>
+            <p className="header-small">You are ready to begin your subscription</p>
+          </div>
+          <div className="graphic">
+            <img src={Images.daiConfirm}/>
+          </div>
+          <div className="subtext">
+            <p className="subtext-bold">What happens next?</p>
+            <p className="subtext-small">Your subscription will now start, and each month 14.00 DAI will be deducted from your Personal Wallet. Once your balance becomes low, we will notify you and will need to top up your account.</p>
+          </div>
+        </div>
+        <div className="button">
+          <p className="begin-subscription">Begin Subscription – $14/month</p>
+        </div>
+      </div>
+    );
+  };
+};
+
+export default BeginSubscription;
