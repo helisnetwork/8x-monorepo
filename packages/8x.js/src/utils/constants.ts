56b5fe13d9f50bbc8f521238d831d7f98b7498c9
Kerman <kermankohli@gmail.com>
2018-09-15 09:26:52 +1000

    Added back src files - woops!

---
+++ packages/8x.js/src/utils/constants.ts
@@ -0,0 +1,6 @@
+export const EXECUTOR_CACHE_KEY = 'Executor';
+export const REGISTRY_CACHE_KEY = 'Registry';
+export const PAYMENT_REGISTRY_CACHE_KEY = 'PaymentRegistry';
+export const VOLUME_SUBSCRIPTION_CACHE_KEY = 'VolumeSubscription';
+export const STAKE_CONTRACT_CACHE_KEY = 'StakeContract';
+export const REQUIREMENTS_CACHE_KEY = 'Requirements';
