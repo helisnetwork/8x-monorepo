bfe60cbe6f6b3a6ddb16805a5e00da4d49694427
Kerman <kermankohli@gmail.com>
2018-09-17 18:03:36 +1000

    Stubs for API classes added in

--- packages/8x.js/src/index.ts
+++ packages/8x.js/src/index.ts
@@ -5,17 +5,21 @@ import * as Web3 from 'web3';
 import Contracts from './services/contracts';
 import PlanAPI from './apis/plan_api';
 import VolumeSubscriptionWrapper from './wrappers/volume_subscription_wrapper';
+import SubscriptionsAPI from './apis/subscriptions_api';
+import ExchangeAPI from './apis/exchange_api';
 
 
 export default class EightEx {
 
   private web3: Web3;
   private addressBook: AddressBook;
+  private contracts: Contracts;
 
   private volumeSubscriptionWrapper: VolumeSubscriptionWrapper;
 
-  public contracts: Contracts;
   public plans: PlanAPI;
+  public subscriptions: SubscriptionsAPI;
+  public exchange: ExchangeAPI;
 
   constructor(web3: Web3, addressBook: AddressBook) {
     this.web3 = web3;
@@ -25,6 +29,8 @@ export default class EightEx {
 
     this.volumeSubscriptionWrapper = new VolumeSubscriptionWrapper(web3, this.contracts);
     this.plans = new PlanAPI(this.contracts, addressBook, this.volumeSubscriptionWrapper);
+    this.subscriptions = new SubscriptionsAPI(this.contracts, addressBook, this.volumeSubscriptionWrapper);
+    this.exchange = new ExchangeAPI(this.contracts, addressBook);
 
   }
 
