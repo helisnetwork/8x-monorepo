19feb7e56a37493007bb44fb6ea547e827084b54
AndyTCF <andy.c.f.98@gmail.com>
2018-09-24 13:17:38 +1000

    Planhash input results in a fetch from eightex

--- packages/payment-gateway/src/store/subscription.js
+++ packages/payment-gateway/src/store/subscription.js
@@ -9,26 +9,60 @@ class SubscriptionStore {
   }
 
   startListening() {
-    this.retrievePlanListener(); 
-    this.authorizationListener();
-    this.subscribeListener(); 
-    this.activateListener();
+    bus.on('web3:initialised', web3 => {
+      console.log(web3);
+      this.eightEx = new EightEx(web3, {
+        volumeSubscriptionAddress: '0xeff7b9ad5594d105a914a6aa8ef270dae343ee63',
+        transactingTokenAddress: '0xc4375b7de8af5a38a93548eb8453a498222c4ff2'
+      });
+      this.storePlanHash(); 
+      this.retrievePlanListener(); 
+    });
+    // this.authorizationListener();
+    // this.subscribeListener(); 
+    // this.activateListener();
+  }
+
+  storePlanHash() {
+    bus.on('planhash:sent', (planHash) => {
+      this.planHash = planHash;
+    });
   }
 
   retrievePlanListener() {
     bus.on('subscription:plan:requested', () => {
-      let subscriptionPlan = 
-        {
-          logo: Images.netflixLogo,
-          subscriptionName: 'Netflix',
-          subscriptionDetails: 'Premium Account',
-          subscriptionAmount: 14,
-          subscriptionPeriod: 'monthly',
+      this.eightEx.plans.get(
+        this.planHash
+      ).then((elem) => {
+        this.currentPlan = Array.from(elem);
+        console.log(elem);
+        if (this.currentPlan) {
+          const planObj = (({ image, name, description, amount, interval }) => ({
+            logo: image,
+            subscriptionName: name,
+            subscriptionDetails: description,
+            subscriptionAmount: amount.c,
+            subscriptionPeriod: interval
+          }))(elem);
+          console.log(planObj); 
+          bus.trigger('subscription:plan:sent', planObj);
         }
-      ;
-
-      bus.trigger('subscription:plan:sent', subscriptionPlan); 
+      });
     });
+
+    // bus.on('subscription:plan:requested', () => {
+    //   let subscriptionPlan = 
+    //     {
+    //       logo: Images.netflixLogo,
+    //       subscriptionName: 'Netflix',
+    //       subscriptionDetails: 'Premium Account',
+    //       subscriptionAmount: 14,
+    //       subscriptionPeriod: 'monthly',
+    //     }
+    //   ;
+
+    //   bus.trigger('subscription:plan:sent', subscriptionPlan); 
+    // });
   };
 
   authorizationListener() {
