a2263e50cfc377ab864d4e05f8c992ead7318611
Kerman <kermankohli@gmail.com>
2018-09-25 20:38:23 +1000

    Basic service node working (tests)

--- packages/repeater/src/index.ts
+++ packages/repeater/src/index.ts
@@ -1,30 +1,43 @@
 import Web3 = require("web3");
 
-import { ExecutorAbi } from '@8xprotocol/artifacts'
-import { AddressBook } from '@8xprotocol/types';
+import { ExecutorAbi, ExecutorContract } from '@8xprotocol/artifacts'
+import { AddressBook, Address } from '@8xprotocol/types';
+
+import SubscriptionEvent from "./types";
 
 import EventStore from './store/events';
+import ProcessorStore from "./store/processor";
 
 export default class Repeater {
 
   private web3: Web3;
-  private addressBook: AddressBook;
+  private executorContract: ExecutorContract;
+
+  private executorAddress: Address;
+  private serviceNodeAccount: Address;
 
   public eventStore: EventStore
+  public processorStore: ProcessorStore;
+
   public repeaterUpdated: () => (void);
 
-  constructor(addressBook: AddressBook, provider?: any, privateKey?: string) {
-    this.web3 = new Web3(provider || privateKey);
-    this.addressBook = addressBook;
-    this.eventStore = new EventStore(this.web3, addressBook.executorAddress || '', () => this.storeUpdated());
+  constructor(web3: Web3, executorAddress: Address, serviceNodeAccount: Address) {
+    this.web3 = web3;
+    this.executorAddress = executorAddress;
+    this.serviceNodeAccount = serviceNodeAccount;
   }
 
   public async start() {
+    this.executorContract = await ExecutorContract.at(this.executorAddress, this.web3, {});
+
+    this.eventStore = new EventStore(this.web3, this.executorContract, () => this.storeUpdated());
+    this.processorStore = new ProcessorStore(this.web3, this.serviceNodeAccount, this.executorContract);
+
     await this.eventStore.startListening();
   }
 
   public storeUpdated() {
-    console.log('Store updated was called');
+    this.processorStore.setEvents(Object.values(this.eventStore.events));
     this.repeaterUpdated();
   }
 
