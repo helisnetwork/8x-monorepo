9c8029c479a9f6d852f34a130a6d7d3e3091aa0c
Kerman <kermankohli@gmail.com>
2018-09-27 15:19:02 +1000

    Move HTML to examples folder

---
+++ packages/8x.pay/examples/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// main app
+import EightExPay from '../../src/index.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  let element = document.getElementById('8x.pay');
+  let planHash;
+
+  if (element.hasAttributes()) {
+    var attrs = element.attributes;
+    for (var i = attrs.length - 1; i >= 0; i--) {
+      if (attrs[i].name == 'planhash') {
+        planHash = attrs[i].value;
+      }
+    }
+  }
+
+  console.log('Plan hash is ' + planHash);
+  ReactDOM.render(
+    <EightExPay
+      planHash={planHash}
+      activated={(subscriptionHash, status) => {
+        console.log(subscriptionHash + ' ' + status);
+      }}/>
+    , element);
+});
--- packages/8x.pay/src/index.js
+++ packages/8x.pay/src/index.js
@@ -1,28 +1,73 @@
+/* Import statements */
 import React from 'react';
-import ReactDOM from 'react-dom';
+import './assets/stylesheets/app.scss';
+import { MemoryRouter } from 'react-router-dom';
+import {
+  TransitionGroup,
+  CSSTransition
+} from 'react-transition-group';
 
-// main app
-import EightExPay from './eight_ex_pay.js';
+import Container from './components/container';
+import ModalLauncher from './components/modal-launcher';
+import UserStore from './store/user';
+import SubscriptionStore from './store/subscription';
 
-document.addEventListener('DOMContentLoaded', () => {
-  let element = document.getElementById('8x.pay');
-  let planHash;
+import bus from './bus';
 
-  if (element.hasAttributes()) {
-    var attrs = element.attributes;
-    for (var i = attrs.length - 1; i >= 0; i--) {
-      if (attrs[i].name == 'planhash') {
-        planHash = attrs[i].value;
+/* App component */
+class EightExPay extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      planHash: ''
+    };
+
+    let userStore = new UserStore();
+    let subscriptionStore = new SubscriptionStore();
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+
+    console.log(`plan triggered: ${props.planHash}`);
+    bus.trigger('planhash:sent', props.planHash);
+
+    bus.on('user:activate:completed', (subscriptionHash, status) => {
+      if (props.activated) {
+        props.activated(subscriptionHash, status);
+        bus.trigger('modal:show', false);
       }
-    }
+    });
+
   }
 
-  console.log('Plan hash is ' + planHash);
-  ReactDOM.render(
-    <EightExPay
-      planHash={planHash}
-      activated={(subscriptionHash, status) => {
-        console.log(subscriptionHash + ' ' + status);
-      }}/>
-    , element);
-});
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+
+    bus.trigger('planhash:sent', value);
+
+  };
+
+
+  render() {
+    return (
+      <div>
+        <ModalLauncher buttonLabel='Pay with 8x'>
+          <MemoryRouter>
+            <Container/>
+          </MemoryRouter>
+        </ModalLauncher>
+      </div>
+
+    );
+  }
+};
+
+export default EightExPay;
+
