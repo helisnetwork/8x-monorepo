3ff9372e1312b148f9166d9ce79905ca91f4b06b
Kerman <kermankohli@gmail.com>
2018-09-29 18:35:46 +1000

    Remove beta-site folder

--- packages/beta-site/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-// main app
-import App from './app.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<App/>, document.getElementById('app'));
-});
