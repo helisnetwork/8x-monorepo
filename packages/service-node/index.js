817737ae396f1e27273b670f836be2bdcde876e7
Kerman <kermankohli@gmail.com>
2018-09-29 18:30:22 +1000

    Updated service node itself

--- packages/service-node/index.js
+++ packages/service-node/index.js
@@ -1,8 +1,37 @@
+/*
+
+------------------------------------------------------------
+
+Create a .env file and fill in the following variables below
+
+------------------------------------------------------------
+
+// Public key of the node executing
+NODE_ADDRESS = ""
+
+// Private key of the node executing
+PRIVATE_KEY = ""
+
+// Executor contract address
+EXECUTOR = ""
+
+// Transcting token address (that you'd like to stake for)
+TRANSACTING_TOKEN = ""
+
+// Token being staked (native 8x token)
+STAKE_TOKEN = ""
+
+// Stake contract address
+STAKE_CONTRACT = ""
+
+*/
+
 const Web3 = require('web3');
 
 const Repeater = require('@8xprotocol/service-node-core').default;
 const dotenv = require('dotenv')
 const HDWalletProvider = require("truffle-hdwallet-provider-privkey");
+const BigNumber = requre('bignumber.js');
 
 const environment = dotenv.config().parsed;
 
@@ -10,19 +39,26 @@ let provider = new Web3.providers.HttpProvider(environment.NODE_ADDRESS);
 
 if (environment.PRIVATE_KEY) {
   provider = new HDWalletProvider([environment.PRIVATE_KEY], environment.NODE_ADDRESS);
-  console.log('Private key set');
+  console.log('Private key set sucessfully');
 }
 
 const web3 = new Web3(provider);
-const repeater = new Repeater(web3, environment.EXECUTOR_ADDRESS, environment.NODE_ADDRESS)
-console.log(environment);
+const repeater = new Repeater(web3, environment.EXECUTOR, environment.NODE_ADDRESS)
 
-repeater.start().then(function() {
-  console.log("Started!");
+repeater.attemptTopUp(
+  new BigNumber(100*10**18),
+  environment.TRANSACTING_TOKEN,
+  environment.STAKE_TOKEN,
+  environment.STAKE_CONTRACT)
+.then(function() {
+  console.log('Top up successfull');
+  return repeater.start()
+}).then(function() {
+  console.log("Started node");
 }).catch(function(error) {
   console.log(error);
 })
 
 repeater.repeaterUpdated = function() {
-  console.log("Repeater updated");
+  console.log("Repeater events updated");
 };
