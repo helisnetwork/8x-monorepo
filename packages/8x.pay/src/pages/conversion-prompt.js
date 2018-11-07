dd53e9ca325f8597f5cc1b807710fc4dbdb1c4e0
AndyTCF <andy.c.f.98@gmail.com>
2018-11-07 14:20:41 +1100

    Kyber conversion page html and styling updated

--- packages/8x.pay/src/pages/conversion-prompt.js
+++ packages/8x.pay/src/pages/conversion-prompt.js
@@ -2,44 +2,78 @@ import React from 'react';
 import Header from '../components/header';
 import { default as Images } from '../middleware/images';
 import { Link } from 'react-router-dom'; 
+import Dropdown from '../components/dropdown';
 
 class ConversionPrompt extends React.Component {
 
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      conversionPeriod: ''
+    };
+
+    this.handleSelectedConversionPeriod = this.handleSelectedConversionPeriod.bind(this);
+
+  }
+
+  handleSelectedConversionPeriod(period) {
+    this.setState({
+      conversionPeriod: period
+    });
+  }
+
+  conversionPeriod() {
+    return [
+      {
+        name: "1 month"
+      },
+      {
+        name: "3 months"
+      },
+      {
+        name: "6 months"
+      },
+      {
+        name: "9 months"
+      }
+    ];
   }
 
   render(){
     return (
       <div className="small-card">
-        <Header title="Convert to Dai" previousPage="/metamask-handler"/>
         <div className="main-content">
           <div className="main-text">
-            <h2 id="message">Once you begin your subscription, your Ethereum will be converted into Dai.</h2>
+            <h1 id="message">Conversion</h1>
+            <p id="sub-message">To fund your future payments, we can convert your Ethereum into Dai. Converting your Dai now will eliminate the need for you to manually top up every month and avoid cancellation.</p>
+          </div>
+          <div className="conversion-dropdown">
+            <p id="dropdown-text">Convert enough Dai for</p>
+            <Dropdown items={this.conversionPeriod()} onSelectedItem={this.handleSelectedConversionPeriod}/>
           </div>
           <div className="main-graphics">
             <div className="ethereum">
               <img className="logo" src={Images.ethLogo}/>
-              <h2>0.014</h2>
-              <p>ETH</p>
+              <p>Send</p>
+              <h2>0.014 ETH</h2>
             </div>
             <div className="arrow">
               <img className="arrow" src={Images.arrow}/>
             </div>
             <div className="dai">
               <img className="logo" src={Images.daiLogo}/>
-              <h2>84.00</h2>
-              <p>DAI</p>
+              <p>Receive</p>
+              <h2>240 DAI</h2>
             </div>
           </div>
           <div className="secondary-text">
-            <p className="title">Why is this happening?</p>
-            <p className="paragraph">DAI is a stablecoin that is pegged to the US Dollar, this means it’s value will always stay around $1.00ea. By converting your ETH to DAI, you will avoid the price fluctuations and will have enough funds to cover your subscription for 6 months.</p>
+            <p className="title">1 ETH = 203.00 DAI</p>
           </div>
         </div>
         <Link to='/begin-subscription'>
           <div className="button">
-            <p className="convert">Convert to DAI</p>
+            <p className="convert">Convert</p>
           </div>
         </Link>
       </div>
