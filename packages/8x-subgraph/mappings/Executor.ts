3a3d200c6122e004017ffcf02991669cd0112a9d
AndyTCF <andy.c.f.98@gmail.com>
2018-11-10 01:28:11 +1100

    Deployed subgraph to 8x

--- packages/8x-subgraph/mappings/Executor.ts
+++ packages/8x-subgraph/mappings/Executor.ts
@@ -4,39 +4,39 @@ import { SubscriptionActivated, SubscriptionProcessed, SubscriptionCancelled } f
 
 
 export function handleSubscriptionActivated(event: SubscriptionActivated): void {
-  let id = event.params.subscriptionIdentifier.toHex();
+  // let id = event.params.subscriptionIdentifier.toHex();
   
-  let subscriptionActivationToken = new SubscriptionActivated();
+  // let subscriptionActivationToken = new SubscriptionActivated();
 
-  subscriptionActivationToken.subscriptionAddress = event.params.subscriptionAddress
-  subscriptionActivationToken.tokenAddress = event.params.tokenAddress
-  subscriptionActivationToken.dueDate = event.params.dueDate
-  subscriptionActivationToken.amount = event.params.amount
-  subscriptionActivationToken.fee = event.params.fee
+  // subscriptionActivationToken.subscriptionAddress = event.params.subscriptionAddress
+  // subscriptionActivationToken.tokenAddress = event.params.tokenAddress
+  // subscriptionActivationToken.dueDate = event.params.dueDate
+  // subscriptionActivationToken.amount = event.params.amount
+  // subscriptionActivationToken.fee = event.params.fee
 
 
-  store.set('SubscriptionActivated', id, subscriptionActivationToken);
+  // store.set('SubscriptionActivated', id, subscriptionActivationToken);
 }
 
 export function handleSubscriptionProcessed(event: SubscriptionProcessed): void {
-  let id = event.params.subscriptionIdentifier.toHex();
+  // let id = event.params.subscriptionIdentifier.toHex();
   
-  let subscriptionProcessedToken = new SubscriptionProcessed();
+  // let subscriptionProcessedToken = new SubscriptionProcessed();
 
-  subscriptionProcessedToken.claimant = event.params.claimant
-  subscriptionProcessedToken.dueDate = event.params.dueDate
-  subscriptionProcessedToken.staked = event.params.staked
+  // subscriptionProcessedToken.claimant = event.params.claimant
+  // subscriptionProcessedToken.dueDate = event.params.dueDate
+  // subscriptionProcessedToken.staked = event.params.staked
 
-  store.set('SubscriptionProcessed', id, subscriptionProcessedToken);
+  // store.set('SubscriptionProcessed', id, subscriptionProcessedToken);
 }
 
 export function handleSubscriptionCancelled(event: SubscriptionCancelled): void {
-  let id = event.params.subscriptionIdentifier.toHex();
+  // let id = event.params.subscriptionIdentifier.toHex();
   
-  let subscriptionCancellationToken = new SubscriptionCancelled();
+  // let subscriptionCancellationToken = new SubscriptionCancelled();
 
-  subscriptionCancellationToken.subscriptionAddress = event.params.subscriptionAddress
+  // subscriptionCancellationToken.subscriptionAddress = event.params.subscriptionAddress
 
 
-  store.set('SubscriptionCancelled', id, subscriptionCancellationToken);
+  // store.set('SubscriptionCancelled', id, subscriptionCancellationToken);
 }
