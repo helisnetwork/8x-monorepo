0b077be2a3ebbb548eb6d7c884a7fe5bcc218775
AndyTCF <andy.c.f.98@gmail.com>
2018-11-14 13:46:34 +1100

    Add files for call back url event management

---
+++ packages/event-manager/src/index.ts
@@ -0,0 +1,72 @@
+import Web3 = require("web3");
+
+import { ExecutorAbi, ExecutorContract, StakeContract, MockTokenContract, Web3Utils, ApprovedRegistryContract, VolumeSubscriptionContract } from '@8xprotocol/artifacts'
+import EightEx from '8x.js';
+import { AddressBook, Address } from '@8xprotocol/types';
+import { BigNumber } from 'bignumber.js';
+
+import EventManagementStore from './event-listener/event';
+
+export default class Repeater {
+
+  private web3: Web3;
+  private eightEx: EightEx;
+  private web3Utils: Web3Utils;
+  private executorContract: ExecutorContract;
+
+  private executorAddress: Address;
+
+  public eventStore: EventManagementStore;
+
+  public repeaterUpdated: () => (void) | null;
+
+  constructor(web3: Web3, executorAddress: Address) {
+    this.web3 = web3;
+    this.web3Utils = new Web3Utils(web3);
+    this.executorAddress = executorAddress;
+    this.eightEx = new EightEx(web3, {});
+  }
+
+  public async start() {
+    this.executorContract = await ExecutorContract.at(this.executorAddress, this.web3, {});
+
+    this.eventStore = new EventStore(this.web3, this.executorContract, () => this.storeUpdated());
+
+    await this.eventStore.startListening();
+  }
+
+  public async attemptTopUp(amount: BigNumber, tokenAddress: Address, stakeTokenAddress: Address, stakeContractAddress: Address) {
+
+    let stakeContract = await StakeContract.at(stakeContractAddress, this.web3, {});
+    let stakeTokenContract = await MockTokenContract.at(stakeTokenAddress, this.web3, {});
+
+    let existingBalance = await stakeContract.getTotalStake.callAsync(this.serviceNodeAccount, tokenAddress);
+
+    if (existingBalance.toNumber() == amount.toNumber()) {
+      console.log("Skipped top up");
+      return '';
+    }
+
+    let approveTx = await stakeTokenContract.approve.sendTransactionAsync(
+      stakeContract.address,
+      amount,
+      {from: this.serviceNodeAccount}
+    );
+
+    await this.eightEx.blockchain.awaitTransactionMinedAsync(approveTx);
+
+    let topupTx = await stakeContract.topUpStake.sendTransactionAsync(
+      amount,
+      tokenAddress,
+      {from: this.serviceNodeAccount}
+    );
+
+    await this.eightEx.blockchain.awaitTransactionMinedAsync(topupTx);
+  }
+
+  public storeUpdated() {
+    this.processorStore.setEvents(Object.values(this.eventStore.events));
+    this.repeaterUpdated();
+  }
+
+}
