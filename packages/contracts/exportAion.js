f38ece2921ed4db2a58448316c50d376e6d67cf6
Kerman Kohli <kermankohli@gmail.com>
2018-11-27 12:00:45 +1100

    AION attempt

--- packages/contracts/exportAion.js
+++ packages/contracts/exportAion.js
@@ -1,6 +1,10 @@
 const fs = require('fs-extra');
 const rimraf = require('rimraf');
 const replace = require('replace-in-file');
+const path = require('path');
+
+const Web3 = require('aion-web3')
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
 // Output of truffle compile
 const CONTRACTS_DIR = './contracts';
@@ -9,15 +13,58 @@ const AION_DIR = './aion/contracts';
 fs.emptyDirSync(AION_DIR);
 fs.copySync(CONTRACTS_DIR, AION_DIR)
 
-const options = {
-    files: `${AION_DIR}/**`,
-    from: /uint256/gi,
-    to: 'uint128',
-  };
-
+// Copy all the files into the new AION folder
 try {
-  const changes = replace.sync(options);
-  console.log('Modified files:', changes.join(', \n'));
+
+    const options = {
+        files: `${AION_DIR}/**`,
+        from: /uint256/gi,
+        to: 'uint128',
+    };
+    
+    const changes = replace.sync(options);
+    console.log('Modified files:', changes.join(', \n'));
 } catch (error) {
-  console.error('Error occurred:', error);
+    console.error('Error occurred:', error);
 }
+
+// Function to recursively find all the files
+const walkSync = (dir, filelist = []) => {
+    fs.readdirSync(dir).forEach(file => {
+      filelist = fs.statSync(path.join(dir, file)).isDirectory()
+        ? walkSync(path.join(dir, file), filelist)
+        : filelist.concat({
+            name: file,
+            path: path.join(dir, file),
+            size: fs.statSync(path.join(dir, file)).size,
+        });
+    });
+    return filelist;
+  }
+
+// Start finding all the files and replace things
+const result = walkSync(AION_DIR);
+
+result.forEach((file) => {
+
+    const options = {
+        files: file.path,
+        from: [
+            /constructor/gi,
+            `pragma solidity 0.4.24`
+        ],
+        to: [
+            `function ${file.name.replace('.sol', '')}`,
+            `pragma solidity 0.4.15`
+        ],
+    };
+    
+    const changes = replace.sync(options);
+
+    const newFile = fs.readFileSync(file.path, "utf8");
+    web3.eth.compileSolidity(newFile).then((res) => {
+        console.log(res);
+    }); 
+
+    console.log(file);
+});
