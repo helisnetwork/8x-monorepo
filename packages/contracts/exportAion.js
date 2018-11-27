0da9e23dd2ce96dd6757bfae8fe5fb7b1caa046e
Kerman Kohli <kermankohli@gmail.com>
2018-11-27 21:09:52 +1100

    Flatten contract

--- packages/contracts/exportAion.js
+++ packages/contracts/exportAion.js
@@ -6,9 +6,25 @@ const path = require('path');
 const Web3 = require('aion-web3')
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
+// The files we finally want at the end
+const flattenFiles = [
+    'Executor',
+    'ApprovedRegistry',
+    'PaymentRegistry',
+    'StakeContract',
+    'MockKyberNetwork',
+    'MultiSigWalletWithTimeLock',
+    'EightExToken',
+    'TransferProxy',
+    'VolumeSubscription',
+    'PayrollSubscription',
+];
+
 // Output of truffle compile
+const PARENTS_DIR = './aion';
 const CONTRACTS_DIR = './contracts';
 const AION_DIR = './aion/contracts';
+const FLAT_DIR = './aion/flattened';
 
 fs.emptyDirSync(AION_DIR);
 fs.copySync(CONTRACTS_DIR, AION_DIR)
@@ -40,7 +56,36 @@ const walkSync = (dir, filelist = []) => {
         });
     });
     return filelist;
-  }
+}
+
+// Function to flatten output
+
+async function flatten(inputFilePath) {
+	const inputFileContent = await fs.readFileSync(inputFilePath, 'utf8')
+	let dir = variables.parentDir + '/'
+	const isAbsolutePath = !dir.startsWith('.')
+	if (!isAbsolutePath) {
+		dir = __dirname + '/' + dir
+	}
+	dir = cleanPath(dir)
+	const path = variables.parentDir + '/**/*.sol'
+	const srcFiles = await getSourceFiles(dir, path)
+	variables.srcFiles = srcFiles
+}
+
+async function getSourceFiles(dir, path) {
+	return await glob(path)
+}
+
+async function replaceImports(inputFileContent, dir) {
+	let outputFileContent = await replaceAllImportsRecursively(inputFileContent, dir)
+	outputFileContent = removeDoubledSolidityVersion(outputFileContent)
+	if (!fs.existsSync(FLAT_DIR)) fs.mkdirSync(FLAT_DIR)
+	const fileName = `_flat.sol`
+	const filePath = `${FLAT_DIR}/${fileName}`
+	fs.writeFileSync(filePath, outputFileContent)
+	log.info(`Success! Flat file ${fileName} is generated to  ${FLAT_DIR} directory`)
+}
 
 // Start finding all the files and replace things
 const result = walkSync(AION_DIR);
@@ -68,6 +113,8 @@ result.forEach((file) => {
     };
     
     const changes = replace.sync(options);
+
+    replaceImports(inputFileContent, dir)
 
     // const newFile = fs.readFileSync(file.path, "utf8");
     // web3.eth.compileSolidity(newFile).then((res) => {
