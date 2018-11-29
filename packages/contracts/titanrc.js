04bf2505b73c313274c3ee61c4d3757ce7adde3a
Kerman Kohli <kermankohli@gmail.com>
2018-11-29 18:29:08 +1100

    Moved scripts into one folder

--- packages/contracts/titanrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-       "host": "https://api.nodesmith.io/v1/aion/mainnet/jsonrpc?apiKey=2a11723e42314e7886056bc3f2157548",
-       "port": 8545,
-       "defaultAccount": "",
-       "password": ""
-   }
