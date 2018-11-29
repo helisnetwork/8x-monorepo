641da2395c6ce4b3309459525d897d332c8107b0
Kerman Kohli <kermankohli@gmail.com>
2018-11-30 10:18:03 +1100

    Minor fixes

---
+++ packages/contracts/aion/test.js
@@ -0,0 +1,38 @@
+const Web3 = require('aion-web3');
+const fs = require('fs');
+
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+// const account = new Accounts();
+
+let account = web3.eth.accounts.privateKeyToAccount('0xa77abf828e0af71551a439174c31e96f19552247cd1e19b1d6322dd61b3df832e17865bbd3b0490940eea10e34a529881932422cf8a19fe8f85aa8ddfff020e7');
+
+(async() => {
+    try {
+        let nameSol = fs.readFileSync("./flat/Executor_flat.sol", "utf8");
+        // Compile the contract
+        let compiled = '';
+        await web3.eth.compileSolidity(nameSol)
+        .then(res => compiled = res)
+        .catch(err => console.log(err))
+
+        console.log(nameSol);
+        console.log('compiled', compiled);
+
+        const contractInst = new web3.eth.Contract(compiled.Executor.info.abiDefinition);
+        const deploy = await contractInst.deploy( {data:compiled.Executor.code, arguments: []} ).encodeABI();
+        const deployTx = { gas: 4000000, gasPrice: 10000000000, data: deploy, from: account.address };
+        await web3.eth.accounts.signTransaction( deployTx, account.privateKey ).then( (res) => signedTx = res );
+
+        console.log(signedTx);
+        await web3.eth.sendSignedTransaction( signedTx.rawTransaction
+          ).on('receipt', receipt => {
+             console.log("Receipt received!\ntxHash =", receipt.transactionHash,
+                         "\ncontractAddress =", receipt.contractAddress);
+             ctAddress = receipt.contractAddress;
+        });
+
+    } catch (err) {
+        console.log(err);
+    }
+})();
+
